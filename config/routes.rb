4a927a2507d578e2dee0e5bc4e7bf8a8e51a9f53
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-10-03 22:54:49 +0300

    DONE

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  # logging in
+    post  "/login", to: "sessions#create"
+    # logging out 
+    delete "/logout", to: "sessions#destroy"
+
+    # 
+  get "/me", to: "users#show"
+
   resources :articles, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
